b80181e3d68132e76caa0915ee9ae4b94dc6f6ac
Felipe Yuji <felipedias0810@gmail.com>
2021-11-28 00:18:18 -0300

    Procedural generation testing

---
+++ Assets/Scripts/RoomBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBehaviour : MonoBehaviour
+{
+    // array pattern will be as follows
+    // 0 - up | 1 - down | 2 - right | 3 - left
+    public GameObject[] walls;
+    public GameObject[] doors;
+    public void UpdateRoom(bool[] status)
+    {
+        for(int i = 0; i < status.Length; i++)
+        {
+            walls[i].SetActive(!status[i]);
+            doors[i].SetActive(status[i]);
+        }
+    }
+
+}
